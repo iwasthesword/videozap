718f5bf96881682a41fc086031f171c84c19f87a
iwasthesword <cinzadiscos@gmail.com>
2022-11-29 15:06:01 -0300

    Updates

---
+++ precache-manifest.cd3bbdc267d13c3a09ae7cb9ca9f8c62.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e0cdf14b6ea108f04cb6be1330429cf",
+    "url": "/videozap/index.html"
+  },
+  {
+    "revision": "0a73f4632e0b67ebcdcc",
+    "url": "/videozap/static/css/main.8ab6f918.chunk.css"
+  },
+  {
+    "revision": "55828ea1a087df15aa94",
+    "url": "/videozap/static/js/2.8975a5ea.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/videozap/static/js/2.8975a5ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a73f4632e0b67ebcdcc",
+    "url": "/videozap/static/js/main.30d1eec4.chunk.js"
+  },
+  {
+    "revision": "959a4f070ecd3b37640d",
+    "url": "/videozap/static/js/runtime-main.9ccf0311.js"
+  }
+]);
